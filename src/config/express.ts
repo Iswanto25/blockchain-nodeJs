83db38943bb3b4a37c66080c8fe964adf6ca5dbf
Iswanto <uncle.dev03@outlook.com>
2025-04-25 19:30:16 +0700

    add moment and @vercel/speed-insights dependencies; enhance request logging with user details

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,16 +1,41 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import express from "express"
 import morgan from "morgan"
+import moment from "moment"
 import routes from "../routes/routes"
-
 
 const app = express()
 
+app.use((req, res, next) => {
+  const forwardedForHeader = req.headers["x-forwarded-for"]
+  const forwardedFor = Array.isArray(forwardedForHeader) ? forwardedForHeader[0] : forwardedForHeader
+  const ip = forwardedFor?.split(",")[0]?.trim() || req.ip
+  const host = `${req.protocol}://${req.get("host")}${req.originalUrl}`
+  const userAgent = req.headers["user-agent"] || "Unknown"
+  const dateTimeNow = moment().format("YYYY-MM-DD HH:mm:ss")
+
+  console.info("Users ip:", ip)
+  console.info("Users host:", host)
+  console.info("Users user-agent:", userAgent)
+  console.info("Users x-forwarded-for:", forwardedFor ?? "undefined")
+  console.info("Timestamp:", dateTimeNow)
+
+  next()
+})
+
 app.use(morgan("dev"))
 app.use(express.json({ limit: "10mb" }))
-app.set('trust proxy', true);
+app.set("trust proxy", true)
 
-
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Welcome to the Blockchain API",
+  })
+})
 app.use("/blockchain", routes)
+app.get("/favicon.ico", (req, res) => {
+  res.status(204).end() // No Content
+})
 
 export default app
-
